14510270f0159f949220d6ddcaa083e9c5775d20
grrl <deiziane_ns@hotmail.com>
2021-04-18 18:30:59 -0300

    organizing styles and add sass

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.scss';
 import Navbar from './Navbar';
 import SearchForm from './SearchForm';
 import Media from './Media';
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/Navbar.scss';
 
 function Navbar() {
   return (
