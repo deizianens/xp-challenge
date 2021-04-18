633b3f5639b00dd93f022f17368dfad841055cc5
grrl <deiziane_ns@hotmail.com>
2021-04-18 19:38:20 -0300

    adds input state value to handle changes

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/SearchForm.scss';
 
 function SearchForm() {
+  const [search, setSearch] = useState('');
+
   return (
     <div>
       <form>
-        <input type='text' className='Search-input' />
+        <input
+          type='text'
+          className='Search-input'
+          name='search'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
       </form>
     </div>
   );
