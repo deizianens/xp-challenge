010486ca39259b379482e79b69cb08e27fce5bfd
grrl <deiziane_ns@hotmail.com>
2021-04-21 17:19:57 -0300

    adds spotify service

---
+++ src/services/spotifyService.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+export const getToken = () => {
+  return axios('https://accounts.spotify.com/api/token', {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization:
+        'Basic ' +
+        btoa(
+          process.env.REACT_APP_CLIENT_ID +
+            ':' +
+            process.env.REACT_APP_CLIENT_SECRET
+        ),
+    },
+    data: 'grant_type=client_credentials',
+    method: 'POST',
+  })
+    .then((token) => token.data.access_token)
+    .catch(() => console.log('erro no token'));
+};
+
+export const getAlbum = (search, token) => {
+  return axios(
+    `https://api.spotify.com/v1/search?q=${search}&type=album&limit=10`,
+    {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  )
+    .then((albums) => albums.data.albums.items)
+    .catch(() => console.log('erro na requisição'));
+};
