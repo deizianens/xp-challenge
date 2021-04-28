bd8af09dfe1163d9d54d7d4d2ba6b8934eee95bd
grrl <deiziane_ns@hotmail.com>
2021-04-27 22:40:51 -0300

    adds react-router-dom

---
+++ src/Album.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/Album.scss';
+
+const Album = () => {
+  return (
+    <div>
+      <h1>Album</h1>
+    </div>
+  );
+};
+
+export default Album;
--- src/App.js
+++ src/App.js
@@ -1,54 +1,14 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './styles/App.scss';
-import Navbar from './Navbar';
-import SearchForm from './SearchForm';
-import Media from './Media';
-import { getToken, getAlbum } from './services/spotifyService';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Album from './Album';
+import Home from './Home';
 
 function App() {
-  const [media, setMedia] = useState([]);
-  const [token, setToken] = useState(undefined);
-  const [isSearching, setIsSearching] = useState(false);
-  const search = useRef();
-
-  useEffect(() => {
-    if (!token) {
-      getToken().then((accessToken) => {
-        setToken(accessToken);
-        getAlbum(accessToken).then((response) => setMedia(response));
-      });
-    }
-  }, []);
-
-  const onSearch = (searchedMedia, isSearching) => {
-    search.current = searchedMedia;
-    setIsSearching(isSearching);
-  };
-
   return (
-    <>
-      <Navbar />
-      <div className='container'>
-        <SearchForm token={token} setMedia={setMedia} onSearch={onSearch} />
-        <section>
-          <h3>
-            {isSearching
-              ? `Resultados encontrados para "${search.current}"`
-              : 'Álbuns buscados recentemente'}
-          </h3>
-          <div className='media'>
-            {media &&
-              media.map((item) => (
-                <Media
-                  cover={item.images[1].url}
-                  title={item.name}
-                  artist={item.artists[0].name}
-                />
-              ))}
-          </div>
-        </section>
-      </div>
-    </>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/album/:name' component={Album} />
+    </Switch>
   );
 }
 
